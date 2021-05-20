8dfae30cacdfd54337ed0ac42984d6795fa0b417
Shahin Safaroff <shahinsafaroff@gmail.com>
2021-05-20 19:46:42 +0400

    First Review_2

--- skillbox_lesson_7_hometask_5/main.go
+++ skillbox_lesson_7_hometask_5/main.go
@@ -14,7 +14,7 @@ func main() {
 		fmt.Println(10-attempts, " tries left ")
 		fmt.Print("Please enter guessed number: \n")
 		_, _ = fmt.Scan(&num)
-		fmt.Print(" You've entered number is : ", num)
+		fmt.Print(" You've entered number is: ", num)
 		if num > target {
 			fmt.Println(" Your guess is higher, enter lesser number")
 		} else if num < target {
