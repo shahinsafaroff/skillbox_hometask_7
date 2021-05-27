3011f6670ee2290b1e1d6ea2988de088504deff4
Shahin Safaroff <shahinsafaroff@gmail.com>
2021-05-28 03:16:15 +0400

    Bang

--- skillbox_lesson_7_hometask_1/main.go
+++ skillbox_lesson_7_hometask_1/main.go
@@ -2,35 +2,18 @@ package main
 import (
 	"fmt"
 )
-func main() {
-var count, i, lastDigit, fifthDigit, fourthDigit, thirdDigit, secondDigit, firstDigit int
-	for i = 100000; i <= 999999; i++ {
-		for firstDigit = 0; firstDigit < 10; firstDigit++ {
-			for secondDigit = 0; secondDigit < 10; secondDigit++ {
-				for thirdDigit = 0; thirdDigit < 10; thirdDigit++ {
-					for fourthDigit = 0; firstDigit < 10; fourthDigit++ {
-						for fifthDigit = 0; fifthDigit < 10; fifthDigit++ {
-							for lastDigit = 0; lastDigit < 10; lastDigit++ {
-								if ((firstDigit + secondDigit + thirdDigit) == (fourthDigit + fifthDigit + lastDigit)) {
-									count++
-									fmt.Println(count)
-								}
-							}
-						}
-					}
-				}
-			}
+func tickets() int {
+var i, sum int
+sum = 0
+for i = 100000; i <= 999999; i++ {
+		if (i/100000%10 == i/100%10 && i/10000%10==i/10%10 && i/1000%10 == i%10 ) {
+			sum++
 		}
 	}
-	//lastDigit= i%10
-	//fifthDigit = (i/10)%10
-	//fourthDigit = (i/100)%10
-	//thirdDigit = (i/1000)%10
-	//secondDigit = (i/10000)%10
-	//firstDigit = (i/100000)%10
-	//if lastDigit == firstDigit && fifthDigit==secondDigit && fourthDigit== thirdDigit{
-	//count++
-	//}
-	//}
-	//fmt.Println( count)
+return sum
+}
+func main() {
+	var b int
+	b = tickets()
+	fmt.Println("Количество счастливых билетов: ", b)
 }
